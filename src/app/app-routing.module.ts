cf2268a384c782b911a60c85eaeae385861ca5f7
Ankur Kejriwal <ankurkejriwal@ankurs-mbp.lan>
2021-07-27 09:45:08 -0400

    Working tic-tac-toe

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GameComponent } from './game/game.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
@@ -9,7 +10,7 @@ const routes: Routes = [
   },
   {
     path: 'game',
-    component: HomeComponent,
+    component: GameComponent,
   },
 ];
 
--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -5,7 +5,7 @@ const { GameStep } = Minimax;
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
-  styleUrls: ['./game.component.css']
+  styleUrls: ['./game.component.scss']
 })
 export class GameComponent {
 
